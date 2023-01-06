8f5947209007ca8e1ccac6f78550cdf0ca63ae47
Mikhail Lyalin <mc.lyalin@gmail.com>
2023-01-06 16:35:03 +0500

    Refactor app

--- qap17_9_1/app.py
+++ qap17_9_1/app.py
@@ -12,11 +12,7 @@ else:
   sorted = sort(items)
   index = binary_search(sorted, num, 0, len(sorted) - 1)
 
-  if not index:
-    print('Искомый индекс не найден.')
-  else:
+  if index:
     print(f'Искомый индекс равен: {index}')
-
-# 1 2 d 4 56 78
-# 23 -45 43 0.5 34
-# 1 2 3 -4 5 7 54
+  else:
+    print('Искомый индекс не найден.')
